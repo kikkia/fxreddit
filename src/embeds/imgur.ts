1f94823f20386389a2a8b30b7ff1af33c863a943
Kikkia <10608318+kikkia@users.noreply.github.com>
2025-03-18 21:34:34 +0900

    properly embed imgur vids/gifs (#1)

--- src/embeds/imgur.ts
+++ src/embeds/imgur.ts
@@ -4,22 +4,24 @@ import { isDefined } from 'remeda';
 import { CACHE_CONFIG } from '../cache';
 
 export async function externalImgurEmbed(post: RedditPost, link: string, head: HTMLElement) {
-    if (link.endsWith('.gif') || link.endsWith('.gifv')) {
-        const html = await fetch(link, { ...CACHE_CONFIG }).then(r => r.text()).then(parseHTML);
-        const clipEmbed = html.querySelector('meta[name="twitter:player"]')?.getAttribute('content');
-        const thumbnail = html.querySelector('meta[name="twitter:image"]')?.getAttribute('content');
-        console.log('here123', thumbnail, link);
-        console.log('w/h', post.resolution?.width, post.resolution?.height);
+    const html = await fetch(link, { ...CACHE_CONFIG }).then(r => r.text()).then(parseHTML);
+    
+    const videoUrl = html.querySelector('meta[property="og:video"]')?.getAttribute('content');
+    const twitterPlayerStream = html.querySelector('meta[name="twitter:player:stream"]')?.getAttribute('content');
+    
+    const videoSource = videoUrl || twitterPlayerStream;
+    
+    const thumbnail = html.querySelector('meta[name="twitter:image"]')?.getAttribute('content');
+        
+    if (videoSource) {
         if (thumbnail) {
             head.image(thumbnail, post.resolution?.width, post.resolution?.height);
         }
-        if (clipEmbed) {
-            head.video(clipEmbed, post.resolution?.width, post.resolution?.height, 'video/mp4');
-        }
-    } else {
-        console.log('here456 %s', post);
-        if (isDefined(post.preview_image_url)) {
-            head.image(post.preview_image_url, post.resolution?.width, post.resolution?.height, 'large');
-        }
+
+        head.video(videoSource, post.resolution?.width, post.resolution?.height, 'video/mp4');
+    } else if (isDefined(post.preview_image_url)) {
+        head.image(post.preview_image_url, post.resolution?.width, post.resolution?.height, 'large');
+    } else if (thumbnail) {
+        head.image(thumbnail, post.resolution?.width, post.resolution?.height, 'large');
     }
 }
